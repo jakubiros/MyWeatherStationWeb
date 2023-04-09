0ca13de0ff5267ddab9a06aefd245732b1c0e719
jakubiros <jakub.biros@microsoft.wsei.edu.pl>
2023-04-09 19:55:03 +0200

    Added new web design and read data from MySQL

--- MyWeatherStation/Data/sensordata.cs
+++ MyWeatherStation/Data/sensordata.cs
@@ -2,11 +2,12 @@
 {
     public class sensordata
     {
-        public int id {  get; set; }
+        public int id { get; set; }
 
         public string temperature { get; set; }
 
         public string pressure { get; set; }
+        //((double.Parse(pressure) * 100) / Math.Pow((1 - (323) / 44330), 5.255) / 100).ToString();
 
         public string humidity { get; set; }
 
